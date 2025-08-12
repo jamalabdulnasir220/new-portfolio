516e33836555874da029beff59283a365a240ef2
unknown <jamalnasirone52@gmail.com>
2025-08-12 11:43:08 +0000

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
             </motion.p>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
+        {/* <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
             <motion.img
               initial={{ x: 100, opacity: 0 }}
@@ -53,7 +53,7 @@ const Hero = () => {
               alt="profile picture"
             />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
